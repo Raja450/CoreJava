fc91fc768e289b245a684e910b92d267d2dc7e73
raja700 <raja.sah290598@gmail>
2021-09-24 18:15:46 +0530

    Runtime Polymorphism

---
+++ src/com/RunTimePollymorphism.java
@@ -0,0 +1,25 @@
+package com;
+
+class P {
+	public void test1() {
+		System.out.println(200);
+	}
+
+	public void test2() {
+		System.out.println(500);
+	}
+}
+
+public class RunTimePollymorphism extends P {
+	@Override
+	public void test1() {
+		System.out.println(200);
+	}
+
+	public static void main(String[] args) {
+		RunTimePollymorphism r1 = new RunTimePollymorphism();
+		r1.test1();
+		r1.test2();
+
+	}
+}
