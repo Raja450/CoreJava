0a3e9c39e34462ad15b199b693cc8bacb6ccbbc6
raja700 <raja.sah290598@gmail>
2021-09-24 18:18:16 +0530

    Throw

---
+++ src/com/Throw.java
@@ -0,0 +1,27 @@
+package com;
+
+import java.util.Scanner;
+
+class InsufficientFund extends Throwable {
+
+}
+
+public class Throw {
+	public static void main(String[] args) {
+		int balance = 500;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the Amount");
+		int amount = s.nextInt();
+		if (balance > amount) {
+			System.out.println("Please Collect the cash");
+		} else {
+			try {
+				throw new InsufficientFund();
+			} catch (InsufficientFund e) {
+				System.out.println(e);
+			}
+			System.out.println("Low Balance");
+		}
+	}
+
+}
