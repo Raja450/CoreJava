0b6d9f975dd437e84bf5afbebda002f284f0ba20
raja700 <raja.sah290598@gmail>
2021-09-24 18:09:16 +0530

    Interface

---
+++ src/com/Interface.java
@@ -0,0 +1,51 @@
+package com;
+
+interface Animal
+{
+	void run();
+
+	void sleep();
+
+}
+
+interface Tiger
+{
+	void walk();
+}
+
+class Lion implements Animal, Tiger
+{
+
+	@Override
+	public void run() {
+		System.out.println("speed 100Kmph");
+
+	}
+
+	@Override
+	public void walk()
+	{
+		System.out.println("slowly");
+
+	}
+
+	@Override
+	public void sleep()
+	{
+		System.out.println("sleep 8 hours");
+
+	}
+
+}
+
+public class Interface extends Lion
+{
+	public static void main(String[] args)
+	{
+		Interface i1 = new Interface();
+		i1.run();
+		i1.walk();
+		i1.sleep();
+	}
+
+}
