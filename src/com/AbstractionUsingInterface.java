4ec4f9ef97a1f321641be0242ed1f4bb0ccd1314
raja700 <raja.sah290598@gmail>
2021-09-24 17:56:45 +0530

    Abstraction Using Interface

---
+++ src/com/AbstractionUsingInterface.java
@@ -0,0 +1,26 @@
+package com;
+
+interface Person {
+	void dsplay();
+}
+
+class Employee implements Person {
+	public void dsplay() {
+		System.out.println("This is display method of the Employee class");
+	}
+}
+
+class Hr implements Person {
+	public void dsplay() {
+		System.out.println("This is display method of the HR class");
+	}
+}
+
+public class AbstractionUsingInterface {
+	public static void main(String args[]) {
+		Person person1 = new Employee();
+		person1.dsplay();
+		Person person2 = new Hr();
+		person2.dsplay();
+	}
+}
