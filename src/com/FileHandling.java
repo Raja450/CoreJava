d0dc025812a23c27e5f27dd3a6473d74af674899
raja700 <raja.sah290598@gmail>
2021-09-24 18:04:16 +0530

    FileHandling

---
+++ src/com/FileHandling.java
@@ -0,0 +1,25 @@
+package com;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileHandling {
+	 public static void main(String[] args) {  
+	      
+		    try {  
+		        FileWriter fwrite = new FileWriter("E:myTestFile.txt");  
+		        // writing the content into the FileOperationExample.txt file  
+		        fwrite.write("I am Raja Sah I am from Kolkata.");     
+		        fwrite.write("\n I have done BCA from Brainware University");
+		        fwrite.write("\n I like to Play Cricket");
+		        
+		   
+		        // Closing the stream  
+		        fwrite.close();   
+		        System.out.println("Content is successfully wrote to the file.");  
+		    } catch (IOException e) {  
+		        System.out.println("Unexpected error occurred");  
+		        e.printStackTrace();  
+		        }  
+		    }  
+		}  
