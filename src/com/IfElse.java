b287955c7f8ab3793e29196a3c9d71697bd865bd
raja700 <raja.sah290598@gmail>
2021-09-24 18:07:56 +0530

    IfElse

---
+++ src/com/IfElse.java
@@ -0,0 +1,16 @@
+package com;
+
+public class IfElse
+{
+	public static void main(String[] args)
+	{
+		int age = 23;
+		if (age == 23)
+		{
+			System.out.println(" My Age");
+		} else
+		{
+			System.out.println("Not My age");
+		}
+	}
+}
