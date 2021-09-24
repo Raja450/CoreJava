d5f2ee7bc653904f657ef804b9510a6dd536a117
raja700 <raja.sah290598@gmail>
2021-09-24 18:16:22 +0530

    Set Concept

---
+++ src/com/SetConcept.java
@@ -0,0 +1,18 @@
+package com;
+
+import java.util.LinkedHashSet;
+
+public class SetConcept {
+	public static void main(String[] args) {
+		LinkedHashSet<Integer> raj = new LinkedHashSet<Integer>();
+		// it contains unique value
+		raj.add(78);
+		raj.add(852);
+		raj.add(962);
+		raj.add(45);
+		raj.add(7565);
+		raj.add(45);// it is duplicate value
+		System.out.println(raj);
+	}
+
+}
