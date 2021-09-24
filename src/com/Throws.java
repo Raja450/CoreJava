4bdbbb9c8fb3cce96bdeab047f6497ec4cdfb604
raja700 <raja.sah290598@gmail>
2021-09-24 17:36:26 +0530

    Throws

---
+++ src/com/Throws.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Throws {
+	int division(int a, int b) throws ArithmeticException {
+		int t = a / b;
+		return t;
+	}
+
+	public static void main(String args[]) {
+		Throws a1 = new Throws();
+		try {
+			System.out.println(a1.division(15, 0));
+		} catch (ArithmeticException e) {
+			System.out.println("Number is not divided");
+		}
+	}
+}
