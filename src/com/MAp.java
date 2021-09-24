34b52c1557bedb723bbb9c433ebc9970cb86ae4b
raja700 <raja.sah290598@gmail>
2021-09-24 18:11:52 +0530

    Map

---
+++ src/com/MAp.java
@@ -0,0 +1,18 @@
+package com;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MAp {
+	public static void main(String[] args) {
+		Map<Integer, String> studentInfo = new HashMap<>();
+		studentInfo.put(16, "Raja");
+		studentInfo.put(18, "Raj");
+		studentInfo.put(19, "Vi");
+		System.out.println(studentInfo);
+		System.out.println(studentInfo.values());
+		System.out.println(studentInfo.keySet());
+		System.out.println(studentInfo.get(16));
+	}
+
+}
