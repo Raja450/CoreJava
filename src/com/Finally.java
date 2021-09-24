f9c834d6af182aea74cc59d3ee8197a54c700483
raja700 <raja.sah290598@gmail>
2021-09-24 18:05:18 +0530

    FinallyKeyword

---
+++ src/com/Finally.java
@@ -0,0 +1,16 @@
+package com;
+
+public class Finally {
+	public static void main(String[] args) {
+		try {
+			// code that generates exception
+			int divideByZero = 5 / 0;
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		finally {
+			System.out.println("This is the finally block");
+		}
+	}
+}
