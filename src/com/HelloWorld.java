c9d420b5c349c394e0096bcd47f47efa613988ee
raja700 <raja.sah290598@gmail>
2021-09-29 11:40:31 +0530

    - Code Format

--- src/com/HelloWorld.java
+++ src/com/HelloWorld.java
@@ -1,7 +1,7 @@
 package com;
 
 public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello QDev");
+    }
 }
