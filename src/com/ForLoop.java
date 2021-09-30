58c386e42562b5065a83d8376020a6f4583032ec
raja700 <raja.sah290598@gmail>
2021-09-30 17:45:24 +0530

    For Loop

--- src/com/ForLoop.java
+++ src/com/ForLoop.java
@@ -1,18 +1,15 @@
 package com;
 
-public class ForLoop
-{
-	public static void main(String[] args)
-	{
+public class ForLoop {
+    public static void main(String[] args) {
 
-		int i, j, n = 5;
-		for (i = 1; i <= n; i++) {
-			for (j = 1; j <= i; j++)
-			{
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-	}
+        int i, j, n = 5;
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 
 }
