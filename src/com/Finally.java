80470572fc9cd1e8b7e1a8d061114657cac91105
raja700 <raja.sah290598@gmail>
2021-09-30 17:42:08 +0530

    Finally

--- src/com/Finally.java
+++ src/com/Finally.java
@@ -1,16 +1,14 @@
 package com;
 
 public class Finally {
-	public static void main(String[] args) {
-		try {
-			// code that generates exception
-			int divideByZero = 5 / 0;
-		}
-		catch (Exception e) {
-			System.out.println(e);
-		}
-		finally {
-			System.out.println("This is the finally block");
-		}
-	}
+    public static void main(String[] args) {
+        try {
+            // code that generates exception
+            int divideByZero = 5 / 0;
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("This is the finally block");
+        }
+    }
 }
