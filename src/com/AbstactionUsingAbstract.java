08724bd4813313534a922077c08f2aa0d070c836
raja700 <raja.sah290598@gmail>
2021-09-30 17:13:44 +0530

    Abstraction

--- src/com/AbstactionUsingAbstract.java
+++ src/com/AbstactionUsingAbstract.java
@@ -2,20 +2,21 @@ package com;
 
 //abstract class
 abstract class Car {
-	abstract void accelerate();
+    abstract void accelerate();
 }
 
 //concrete class
 class Suzuki extends Car {
-	void accelerate() {
-		System.out.println("Suzuki::accelerate");
+    void accelerate() {
+        System.out.println("Suzuki::accelerate");
 
-	}
+    }
 }
 
 public class AbstactionUsingAbstract {
-	public static void main(String args[]) {
-		Car obj = new Suzuki();
-		obj.accelerate(); // call the method
-	}
+    public static void main(String args[]) {
+        Car obj = new Suzuki();
+        obj.accelerate(); // call the method
+    }
+}
 }
