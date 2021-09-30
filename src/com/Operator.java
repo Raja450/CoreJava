a0f9ccee4889e87c352779be5ae5d1d253ec6b98
raja700 <raja.sah290598@gmail>
2021-09-30 18:05:17 +0530

    Operator

--- src/com/Operator.java
+++ src/com/Operator.java
@@ -3,20 +3,20 @@ package com;
 import java.util.Scanner;
 
 public class Operator {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		double principal, rate, time, emi;
-		System.out.println("Enter  Principle");
-		principal = sc.nextDouble();
-		System.out.println("Enter the rate");
-		rate = sc.nextDouble();
-		System.out.println("Enter the time in Year");
-		time = sc.nextDouble();
-		rate = rate / (15 * 100);
-		time = time * 12;
-		emi = (principal * rate * Math.pow(1 + rate, time)) / (Math.pow(1 + rate, time) - 1);
-		System.out.print("Monthly EMI is= " + emi + "\n");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double principal, rate, time, emi;
+        System.out.println("Enter  Principle");
+        principal = sc.nextDouble();
+        System.out.println("Enter the rate");
+        rate = sc.nextDouble();
+        System.out.println("Enter the time in Year");
+        time = sc.nextDouble();
+        rate = rate / (15 * 100);
+        time = time * 12;
+        emi = (principal * rate * Math.pow(1 + rate, time)) / (Math.pow(1 + rate, time) - 1);
+        System.out.print("Monthly EMI is= " + emi + "\n");
 
-	}
+    }
 
 }
