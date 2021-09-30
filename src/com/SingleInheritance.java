d3abdedd14e2002d50fd98e34cb3502fac0a852d
raja700 <raja.sah290598@gmail>
2021-09-30 18:17:02 +0530

    Single-Inheritance

--- src/com/SingleInheritance.java
+++ src/com/SingleInheritance.java
@@ -2,18 +2,18 @@ package com;
 
 //base class
 class A {
-	public void test() {
-		System.out.println("Hello Base Class");
-		int a = 10;
-		int b = 20;
-		System.out.println("Sum of two number " + (a + b));
-	}
+    public void test() {
+        System.out.println("Hello Base Class");
+        int a = 10;
+        int b = 20;
+        System.out.println("Sum of two number " + (a + b));
+    }
 }
 
 public class SingleInheritance extends A {
-	public static void main(String[] args) {
-		SingleInheritance a1 = new SingleInheritance();
-		a1.test();
-	}
+    public static void main(String[] args) {
+        SingleInheritance a1 = new SingleInheritance();
+        a1.test();
+    }
 
 }
