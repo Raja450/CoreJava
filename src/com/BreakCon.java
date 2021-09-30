2036e43c153ce44539ff82c4ec7badfd6bd54ea6
raja700 <raja.sah290598@gmail>
2021-09-30 17:23:36 +0530

    Break and Continue

--- src/com/BreakCon.java
+++ src/com/BreakCon.java
@@ -1,17 +1,17 @@
 package com;
 
 public class BreakCon {
-	public static void main(String[] args) {
-		int i, n = 10;
-		for (i = 1; i <= n; i++) {
-			if (i == 8) {
-				break;
-			}
-			if (i == 5) {
-				continue;
-			}
-			System.out.println(i);
-		}
-	}
+    public static void main(String[] args) {
+        int i, n = 10;
+        for (i = 1; i <= n; i++) {
+            if (i == 8) {
+                break;
+            }
+            if (i == 5) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
 
 }
