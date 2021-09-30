49763d471c834c9813cc8b108820d7604537e809
raja700 <raja.sah290598@gmail>
2021-09-30 17:26:06 +0530

    Comparable

--- src/com/ComparableConcept.java
+++ src/com/ComparableConcept.java
@@ -1,64 +1,64 @@
 package com;
 
-class Student implements Comparable<Student> {
-	private String name;
-	private int gpa;
-	private int regNo;
+class Student implements Comparable < Student > {
+    private String name;
+    private int gpa;
+    private int regNo;
 
-	// Constructor
-	public Student(String name, int gpa, int regNo) {
-		this.name = name;
-		this.gpa = gpa;
-		this.regNo = regNo;
-	}
+    // Constructor
+    public Student(String name, int gpa, int regNo) {
+        this.name = name;
+        this.gpa = gpa;
+        this.regNo = regNo;
+    }
 
-	// Override the comapareTo() method
-	@Override
-	public int compareTo(Student other) {
-		return (int) (this.getGpa() - other.getGpa());// compare using GPA
-	}
+    // Override the comapareTo() method
+    @Override
+    public int compareTo(Student other) {
+        return (int)(this.getGpa() - other.getGpa()); // compare using GPA
+    }
 
-	// getters and setters
-	public String getName() {
-		return name;
-	}
+    // getters and setters
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public int getGpa() {
-		return gpa;
-	}
+    public int getGpa() {
+        return gpa;
+    }
 
-	public void setGpa(int gpa) {
-		this.gpa = gpa;
-	}
+    public void setGpa(int gpa) {
+        this.gpa = gpa;
+    }
 
-	public int getRegNo() {
-		return regNo;
-	}
+    public int getRegNo() {
+        return regNo;
+    }
 
-	public void setRegNo(int regNo) {
-		this.regNo = regNo;
-	}
+    public void setRegNo(int regNo) {
+        this.regNo = regNo;
+    }
 }
 
 public class ComparableConcept {
-	public static void main(String[] args) {
-		Student s1 = new Student("Raja", 9, 101);
-		Student s2 = new Student("Bikash", 7, 102);
+    public static void main(String[] args) {
+        Student s1 = new Student("Raja", 9, 101);
+        Student s2 = new Student("Bikash", 7, 102);
 
-		int comparisonResult = s1.compareTo(s2);
+        int comparisonResult = s1.compareTo(s2);
 
-		if (comparisonResult > 0)
-			System.out.println(s1.getName() + " has higher GPA");
+        if (comparisonResult > 0)
+            System.out.println(s1.getName() + " has higher GPA");
 
-		else if (comparisonResult < 0)
-			System.out.println(s2.getName() + " has higher GPA");
+        else if (comparisonResult < 0)
+            System.out.println(s2.getName() + " has higher GPA");
 
-		else
-			System.out.println(s1.getName() + " and " + s2.getName() + " have same GPA");
-	}
+        else
+            System.out.println(s1.getName() + " and " + s2.getName() + " have same GPA");
+    }
 
 }
