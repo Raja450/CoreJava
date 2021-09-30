5e2f42bcc9127b322f2e47d8cc0e59c35b8e9639
raja700 <raja.sah290598@gmail>
2021-09-30 17:56:37 +0530

    List

--- src/com/ListAdd.java
+++ src/com/ListAdd.java
@@ -7,13 +7,13 @@ import java.util.ListIterator;
 import java.util.Arrays;
 
 public class ListAdd {
-	public static void main(String[] args) {
-		List<String> list = Arrays.asList("Hello", "world", "Raja", "18");
-		Iterator<String> itr = list.iterator();
-		while (itr.hasNext()) {
-			System.out.println(itr.next());
+    public static void main(String[] args) {
+        List < String > list = Arrays.asList("Hello", "world", "Raja", "18");
+        Iterator < String > itr = list.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
 
-		}
+        }
 
-	}
+    }
 }
