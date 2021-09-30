91cecc67052cca2eef578c443531ba25b60f2bdf
raja700 <raja.sah290598@gmail>
2021-09-30 17:59:29 +0530

    While loop / do-while loop

--- src/com/LoopWD.java
+++ src/com/LoopWD.java
@@ -1,21 +1,21 @@
 package com;
 
 public class LoopWD {
-	public static void main(String[] args) {
-		// while loop
-		int i = 1;
-		while (i <= 10) {
-			System.out.println(i);
-			i++;
-		}
-		System.out.println("do while ");
-		// do while loop
-		int j = 1;
-		do {
-			System.out.println(j);
-			j++;
+    public static void main(String[] args) {
+        // while loop
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("do while ");
+        // do while loop
+        int j = 1;
+        do {
+            System.out.println(j);
+            j++;
 
-		} while (j <= 10);
-	}
+        } while (j <= 10);
+    }
 
 }
