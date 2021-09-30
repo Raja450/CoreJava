fae9481d8c1c25d172a629ad5255cb4b40d25c94
raja700 <raja.sah290598@gmail>
2021-09-30 18:01:03 +0530

    Map

--- src/com/MAp.java
+++ src/com/MAp.java
@@ -4,15 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MAp {
-	public static void main(String[] args) {
-		Map<Integer, String> studentInfo = new HashMap<>();
-		studentInfo.put(16, "Raja");
-		studentInfo.put(18, "Raj");
-		studentInfo.put(19, "Vi");
-		System.out.println(studentInfo);
-		System.out.println(studentInfo.values());
-		System.out.println(studentInfo.keySet());
-		System.out.println(studentInfo.get(16));
-	}
+    public static void main(String[] args) {
+        Map < Integer, String > studentInfo = new HashMap < > ();
+        studentInfo.put(16, "Raja");
+        studentInfo.put(18, "Raj");
+        studentInfo.put(19, "Vi");
+        System.out.println(studentInfo);
+        System.out.println(studentInfo.values());
+        System.out.println(studentInfo.keySet());
+        System.out.println(studentInfo.get(16));
+    }
 
 }
